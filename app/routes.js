a51520022c14527f156ae57e5d95ba13f88f17c1
nsuska <97541053+nsuska@users.noreply.github.com>
2022-06-13 18:13:26 +0100

    add

--- app/routes.js
+++ app/routes.js
@@ -3,23 +3,30 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+//////////                VERSION 1
+//Location
 
-//LOGIN Q
-
-router.post('/login1', function (req, res) {
-    var login = req.session.data['login1'];
-    switch (login) {
-        case undefined:
-            res.redirect('/login');
-            break;
-            case "teamleader":
-            res.redirect('/teamleader/main-menu');
-            break;
-            case "agent":
-            res.redirect('/agent/main-menu');
-            break;
-            case "manager":
-            res.redirect('/manager/main-menu');
+router.post('/phoenix/v1/location', function (req, res) {
+    const register = req.session.data['register'];
+    if (register === "yes") {
+            res.redirect('/phoenix/v1/business-size');
+    } else {
+            res.redirect('/phoenix/v1/not-eligible');
     };
-});
+})
+
+//business-size
+
+router.post('/phoenix/v1/business-size', function (req, res) {
+    const size = req.session.data['size'];
+    if (size === "micro") {
+            res.redirect('/phoenix/v1/turnover');
+    } if (size === "small") {
+                    res.redirect('/phoenix/v1/turnover');
+    } if (size === "medium") {
+            res.redirect('/phoenix/v1/turnover');
+    } else {
+            res.redirect('/phoenix/v1/not-eligible');
+    };
+})
 module.exports = router
