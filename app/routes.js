a5274044966b52413bb41afa27a37ed92d812ea2
nsuska <97541053+nsuska@users.noreply.github.com>
2022-06-16 14:10:44 +0100

    add

--- app/routes.js
+++ app/routes.js
@@ -27,4 +27,26 @@ router.post('/phoenix/v1/business-size', function (req, res) {
             res.redirect('/phoenix/v1/not-eligible');
     };
 })
+
+
+//////////           PROVIDER VERSION 1
+//seqosh
+
+router.post('/phoenix/provider/v1/seqosh', function (req, res) {
+    const seqosh = req.session.data['seqosh'];
+     if (seqosh === "yes") {
+                    res.redirect('/phoenix/provider/v1/seqosh-number');
+    }if (seqosh === "no") {
+                    res.redirect('/phoenix/provider/v1/gdpr');
+
+    } else {
+            res.redirect('/phoenix/provider/v1/gdpr');
+    };
+})
+
+
+
+
+
+
 module.exports = router
